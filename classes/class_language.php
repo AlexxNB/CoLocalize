21868eb23a6bfe75b3f62097bf4c2d8c58c22c2b
Alexey Schebelev <iam@alexxnb.ru>
2018-03-23 18:37:09 +0300

    Added multilanguage support

---
+++ classes/class_language.php
@@ -0,0 +1,48 @@
+<?php
+class Language {
+    var $_delimeter = ':';
+
+    public function __construct(){
+    }
+
+    public function GetLangVars($lang,$baselang='en'){
+        $BaseStrings = $this->_getLangFile($baselang);
+        if(!$BaseStrings) return array();
+
+        if($lang == $baselang) 
+            $Strings=$BaseStrings;
+        else
+            $Strings = $this->_getLangFile($baselang);
+
+        if(!$Strings) $Strings=$BaseStrings;
+        
+        return $this->_makeLangStructure($String,$BaseStrings,'');
+    }
+
+    public function _makeLangStructure($strings,$baseStrings,$parent){
+        if($parent != '') $parent = $parent.$this->_delimeter;
+        $Struct = array();
+        foreach($baseStrings as $key=>$value){
+            if(is_array($value)){
+               $Struct =  array_merge($Struct,$this->_makeLangStructure($strings[$key],$baseStrings[$key],$parent.$key));
+            }else{
+                if(isset($strings[$key]))
+                    $Struct[$parent.$key]=$strings[$key];
+                else
+                    $Struct[$parent.$key]=$value;
+            }
+        }
+        return $Struct;
+    }
+
+    private function _getLangFile($lang){
+        $path = __DIR__."/../res/lang/$lang.json";
+
+        if(!file_exists($path)) return false;
+
+        $file = file_get_contents($path);
+        return json_decode($file,true);
+    }
+
+}
+?>
