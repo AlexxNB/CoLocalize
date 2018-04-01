07b72dc29ed37303f3eca3272b3275ee666916ca
Alexey Schebelev <iam@alexxnb.ru>
2018-04-01 11:56:00 +0300

    Projects: Done project's list

--- res/views/projects_list.tpl.php
+++ res/views/projects_list.tpl.php
@@ -1,4 +1,24 @@
 <?php if($Projects):?>
+  <a class="btn btn-primary btn-sm float-right" href="/projects/add/"><i class="icon-add"></i> Create Project</a>
+  <h3><?=$L['projects:list:title']?></h3>
+  <div class="mt"></div>
+
+  <?php foreach($Projects as $project):?>
+
+  <div class="tile project-tile">
+    <div class="tile-icon">
+        <div class="progress-icon">100%</div>
+    </div>
+    <div class="tile-content">
+      <p class="title"><a href="/languages/<?=$project['id']?>/"><?=$project['title']?></a></p>
+      <p class="descr"><?=$project['descr']?></p>
+    </div>
+    <div class="tile-action">
+      <a class="btn" href="/projects/edit/<?=$project['id']?>/"><i class="icon-edit"></i> <?=$L['projects:list:edit']?></a>
+      <button class="btn"><i class="icon-delete"></i> <?=$L['projects:list:delete']?></button>
+    </div>
+  </div>
+  <?php endforeach;?>
 
 <?php else: ?>
 <div class="empty">
